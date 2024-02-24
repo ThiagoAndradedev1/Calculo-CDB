a70ee273072a8010181e9409c4ce2e8d4831a806
Thiago <thiago.andrade.dev1@gmail.com>
2024-02-24 10:23:12 -0300

    Inclui lógica inicial para calculo do CDB

--- src/app/data/services/cdb-simulator.service.ts
+++ src/app/data/services/cdb-simulator.service.ts
@@ -7,11 +7,37 @@ import { CdbResult } from '../schemas';
   providedIn: 'root',
 })
 export class CdbSimulatorService {
-  private readonly tb = 0;
+  private readonly tb = 108 / 100;
 
-  private readonly cdi = 0;
+  private readonly cdi = 0.9 / 100;
 
-  simulateCdb(): Observable<CdbResult> {
-    return of<CdbResult>({ totalValue: 0, discountedValue: 0 });
+  private readonly cdiTb = this.tb * this.cdi;
+
+  discountValue(totalValue: number, months: number): number {
+    if (months <= 6) {
+      return totalValue - totalValue * (22.5 / 100);
+    }
+
+    if (months > 6 && months <= 12) {
+      return totalValue - totalValue * (20 / 100);
+    }
+
+    if (months > 12 && months <= 24) {
+      return totalValue - totalValue * (17.5 / 100);
+    }
+
+    if (months > 24) {
+      return totalValue - totalValue * (15 / 100);
+    }
+
+    return 0;
+  }
+
+  simulateCdb(initialValue: number, months: number): Observable<CdbResult> {
+    const totalValue = initialValue * (1 + this.cdiTb);
+
+    const discountedValue = this.discountValue(totalValue, months);
+
+    return of<CdbResult>({ totalValue, discountedValue });
   }
 }
