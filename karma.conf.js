272ba82f3cf2ed04f609bbabccd830e86a3cb9d9
Thiago <thiago.andrade.dev1@gmail.com>
2024-02-25 13:53:09 -0300

    Inclui chrome  na lista de browsers do karma

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chromium'],
+    browsers: ['Chrome','Chromium'],
     restartOnFileChange: true
   });
 };
