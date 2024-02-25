44c3502c4fa682eb9524f5ad3f09abe2eab0aeb4
Thiago <thiago.andrade.dev1@gmail.com>
2024-02-25 14:37:19 -0300

    Inmclui patch no control em numberOnlyDirective

--- src/app/shared/directives/number-only.directive.ts
+++ src/app/shared/directives/number-only.directive.ts
@@ -1,15 +1,17 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appNumberOnly]',
   standalone: true,
 })
 export class NumberOnlyDirective {
-  constructor(private _el: ElementRef) {}
+  constructor(private _el: ElementRef, private _ngControl: NgControl) {}
 
   @HostListener('input', ['$event'])
   onInputChange(): void {
     const initalValue = this._el.nativeElement.value;
     this._el.nativeElement.value = initalValue.replace(/\D*/g, '');
+    this._ngControl.control?.patchValue(initalValue.replace(/\D*/g, ''));
   }
 }
